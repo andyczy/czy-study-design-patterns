3f8263e2360ec02c4cfa5f4d76cf68626bf28436
czy <649954910@qq.com>
2018-08-29 14:31:44 +0800

    简单工厂模式

---
+++ src/main/java/factory/README.md
@@ -0,0 +1,6 @@
+# 工厂设计模式
+
+创建者与调用者分离
+
+![](https://github.com/andyczy/czy-study-design-patterns/blob/master/src/main/java/factory/img/factory1.png "工厂设计模式")
+ 
---
+++ src/main/java/factory/simplefactory/Audi.java
@@ -0,0 +1,10 @@
+package factory.simplefactory;
+
+public class Audi implements Car {
+
+	@Override
+	public void run() {
+		System.out.println("奥迪再跑！");
+	}
+
+}
